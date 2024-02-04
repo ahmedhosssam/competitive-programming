152d27edc05705dd5678e2fcdebb471aea5812a2
Ahmed Hossam <hoss4614@gmail.com>
2024-02-04 19:47:36 +0200

    CF102-D2-B

---
+++ CF-D2-B/CF102-D2-B.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <cmath>
+using namespace std;
+
+#define int long long
+
+int32_t main() {
+    //char c = '6';
+    string str; cin >> str;
+    int res = 0;
+
+    while (str.size() > 1) {
+        int gg = 0;
+        for (int i = 0; i < str.size() ; i++) {
+            gg += str[i] - '0';
+        }
+        str = to_string(gg);
+        res++;
+    }
+    cout << res << endl;
+    return 0;
+}
