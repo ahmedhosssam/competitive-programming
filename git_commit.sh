9a1be8c50fbb303de8aca1659c2e4ade70cab59a
Ahmed Hossam <hoss4614@gmail.com>
2024-09-30 15:40:53 +0300

    1727700053

--- git_commit.sh
+++ git_commit.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-read commit_message
-
-if [ -z "$commit_message" ]; then
-    echo "Commit message cannot be empty. Exiting."
-    exit 1
-fi
-
 git add .
-git commit -am "$commit_message"
+git commit -am "$(date +%s)"
 git push origin main
